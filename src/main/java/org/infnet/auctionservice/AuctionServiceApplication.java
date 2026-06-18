fd489d07d457852667e119f3e223d939abc89d42
pedro <pdrovncs@gmail.com>
2026-06-18 20:56:17 -0300

    adicionado compose 🎠

--- src/main/java/org/infnet/auctionservice/AuctionServiceApplication.java
+++ src/main/java/org/infnet/auctionservice/AuctionServiceApplication.java
@@ -2,12 +2,14 @@ package org.infnet.auctionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@EnableDiscoveryClient
 public class AuctionServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuctionServiceApplication.class, args);
