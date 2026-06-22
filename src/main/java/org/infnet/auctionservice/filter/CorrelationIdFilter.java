75407706d491bbf330180e85ab47f9fab3bfcb7b
pedro <pdrovncs@gmail.com>
2026-06-22 17:09:46 -0300

    adicionado filter, util de correlation e config do logback

---
+++ src/main/java/org/infnet/auctionservice/filter/CorrelationIdFilter.java
@@ -0,0 +1,56 @@
+package org.infnet.auctionservice.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.infnet.auctionservice.utils.CorrelationIdUtil;
+import org.jspecify.annotations.NonNull;
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@Order(1)
+public class CorrelationIdFilter extends OncePerRequestFilter {
+
+    private static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try {
+            String path = request.getRequestURI();
+            if (path.startsWith("/actuator/")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String correlationId = request.getHeader(CORRELATION_ID_HEADER);
+
+            if (correlationId == null || correlationId.isEmpty()) {
+                correlationId = CorrelationIdUtil.generateCorrelationId();
+                log.debug("Correlation ID gerado internamente: {}", correlationId);
+            } else {
+                log.debug("Correlation ID recebido do Gateway: {}", correlationId);
+            }
+
+            MDC.put("correlationId", correlationId);
+
+            response.setHeader(CORRELATION_ID_HEADER, correlationId);
+
+            filterChain.doFilter(request, response);
+
+        } finally {
+            MDC.remove("correlationId");
+        }
+    }
+}
